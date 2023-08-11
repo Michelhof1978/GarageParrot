42f763dfbe033660d5dd54f442a4eb9bf7fc20a5
hoffmann michel <michel.hof@hotmail.fr>
2023-08-11 07:51:06 +0200

    création de la navbar en bootstrap

---
+++ garage/src/containers/Site/Site.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Site extends Component {
+    render() {
+      return (
+        <>
+          SITE
+        </>
+      );
+    }
+  }
+  
+  export default Site;
