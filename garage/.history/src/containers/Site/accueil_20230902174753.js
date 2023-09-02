5ef7a1970ea953795347b3e730c1639806b85fc9
hoffmann michel <michel.hof@hotmail.fr>
2023-09-02 17:51:38 +0200

    test

--- garage/.history/src/containers/Site/accueil_20230902174753.js
+++ garage/.history/src/containers/Site/accueil_20230902174753.js
@@ -3,9 +3,17 @@ import TitreH1 from "../../components/UI/TitreH1/TitreH1";
 import CarousselCard from "../../components/Caroussel/caroussel";
 import peugeot208 from "../../assets/images/peugeot208.webp";
 import logoVoiture from "../../assets/images/logoVoiture.webp";
+<<<<<<< HEAD
 import VehiculeList from "../../components/Vehicules/VehiculeList";
 
 class Accueil extends Component {
+  //Nom de la page ds onglet
+=======
+import VehiculeCard from "../../components/Vehicules/VehiculeCard"; // Assurez-vous que le chemin est correct
+
+class Accueil extends Component {
+  // Nom de la page dans l'onglet
+
   componentDidMount = () => {
     document.title = "Accueil";
   };
@@ -21,22 +29,43 @@ class Accueil extends Component {
               Les réparateurs s’occupent de la réparation et l’entretien de
               votre voiture, peu importe la marque ou le modèle de celle-ci.
               Profitez d’une prestation de qualité effectuée par des véritables
+<<<<<<< HEAD
               experts auto. Contactez nous dès maintenant pour un devis en ligne
+=======
               experts auto. Contactez-nous dès maintenant pour un devis en ligne
+>>>>>>> 004625550824244cfb7c22ebdcd3132c8a18505b
               pour la réparation de votre voiture et obtenez un RDV immédiat !
             </p>
 
             <CarousselCard />
 
-            <VehiculeList />
+<<<<<<< HEAD
+            <VehiculeList/>
+           
 
-            <img className="img-fluid" src={peugeot208} alt="Logo Garage PARROT" />
+            <img
+              className="img-fluid"
+              src={peugeot208}
+              alt="Logo Garage PARROT"
+            />
 
-            <VehiculeCard />
+            <img
+              className="img-fluid"
+=======
+            <VehiculeCard /> 
+           
+            <img
+              className="img-fluid"
+              src={peugeot208}
+              alt="Logo Garage PARROT"
+            />
 
-            <img className="img-fluid" src={peugeot208} alt="Logo Garage PARROT" />
-
-            <img className="img-fluid" src={logoVoiture} alt="Logo Garage PARROT" />
+            <img
+              className="img-fluid"
+>>>>>>> 004625550824244cfb7c22ebdcd3132c8a18505b
+              src={logoVoiture}
+              alt="Logo Garage PARROT"
+            />
           </div>
         </div>
       </>
---
+++ garage/.history/src/containers/Site/voitureoccasion_20230902174641.js
@@ -0,0 +1,75 @@
+
+import React, { Component } from "react";
+import SearchFilters from "../../components/Vehicules/VehiculeFilters";
+import TitreH1 from "../../components/UI/TitreH1/TitreH1";
+
+
+import logoVoiture from "../../assets/images/logoVoiture.webp";
+import bannierevente from "../../assets/images/bannierevente.webp";
+
+class Carsearch extends Component {
+
+  //Nom de la page ds onglet
+  componentDidMount = () => {
+    document.title = "Voiture Occasion";
+  };
+
+  render() {
+    return (
+      <>
+
+  componentDidMount() {
+    document.title = "Voiture Occasion";
+  }
+
+  render() {
+    return (
+      <div>
+
+        <img
+          className="img-fluid w-50"
+          src={bannierevente}
+          alt="Logo Garage PARROT"
+        />
+
+        <div>
+          <TitreH1>Recherche Véhicules</TitreH1>
+
+          <div className="container">
+            <p>
+              Les réparateurs s’occupent de la réparation et l’entretien de
+              votre voiture, peu importe la marque ou le modèle de celle-ci.
+              Profitez d’une prestation de qualité effectuée par des véritables
+              experts auto. Contactez nous dès maintenant pour un devis en ligne
+              pour la réparation de votre voiture et obtenez un RDV immédiat !
+            </p>
+            <SearchFilters />
+            <SearchFilters />
+
+            <img
+              className="img-fluid"
+              src={logoVoiture}
+              alt="Logo Garage PARROT"
+            />
+          </div>
+        </div>
+      </>
+
+        <div className="container">
+          <TitreH1>Recherche Véhicules</TitreH1>
+          <p>
+            Les réparateurs s’occupent de la réparation et l’entretien de votre voiture, peu importe la marque ou le modèle de celle-ci. Profitez d’une prestation de qualité effectuée par des véritables experts auto. Contactez nous dès maintenant pour un devis en ligne pour la réparation de votre voiture et obtenez un RDV immédiat !
+          </p>
+          <SearchFilters />
+        </div>
+        <img
+          className="img-fluid"
+          src={logoVoiture}
+          alt="Logo Garage PARROT"
+        />
+
+    );
+  }
+}
+
+export default Carsearch;
