50a7e1f9623261463d92f595f2bc647aaa0d1c65
hoffmann michel <michel.hof@hotmail.fr>
2023-09-11 16:29:40 +0200

    finalisation des composant fils test ok et visuel ok, maintenant à relier les composants aux véhicules

---
+++ garage/.history/src/components/Filters/BasicSelect_20230911115928.js
@@ -0,0 +1,37 @@
+import "../../App.css";
+
+const BasicSelect = (props) => {
+  
+  const handleChange = (event) => {
+      const {value, name} = event.target; //récupérer la valeur et name du select
+  };
+
+  return (
+    <>
+      <label>{props.label}</label>
+      <select
+        name={props.name}
+        
+        onChange={props.handleChange}
+      >
+
+      {props.options.map((ele, idx) => { //element , index
+
+        if(
+            idx = 0
+        
+        )
+
+        {   return(<option selected value={ele.value}>{ele.text}</option>) 
+        
+         }else{ return(<option value={ele.value}>{ele.text}</option>) }
+
+      })
+      }
+ 
+      </select>
+    </>
+  );
+};
+
+export default BasicSelect;
