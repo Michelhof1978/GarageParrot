968ef3fd16eb90f6d246bf7fc66a851a9470f715
HOFFMANN MICHEL <michel.hof@hotmail.fr>
2023-11-03 13:57:57 +0100

    problème post avis

---
+++ .history/src/components/Avis/DetailsInfoAvisNoteSur5_20231103091320.js
@@ -0,0 +1,17 @@
+//pour afficher la note et le nombre d'avis souhaités
+import React from 'react';
+import "../../App.css";
+
+const  = ({ ratingValue, ratingCount }) => {
+  return (
+    <div>
+      <span className="sr-only"> La note actuelle est de </span>
+      <span itemProp="ratingValue">{ratingValue}</span>
+      <span className="sr-only"> sur </span>
+      <span aria-hidden="true">/</span>{' '}
+      <span itemProp="ratingCount">{ratingCount}</span> avis
+    </div>
+  );
+};
+
+export default DetailsInfoAvisNoteSur5;
---
+++ .history/src/components/Avis/Rating_20231103084253.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import "../../App.css";
+import { useState } from 'react';
+import { Rating } from 'react-simple-star-rating';
+
