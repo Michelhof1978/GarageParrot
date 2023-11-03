f9795f9dd33759606cbf8b18caccad07b1fced1a
HOFFMANN MICHEL <michel.hof@hotmail.fr>
2023-11-03 14:12:03 +0100

    problème post avis

---
+++ garage/.history/src/components/Avis/Rating_20231103082058.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import "../../App.css";
+import { useState } from 'react';
+import { Rating } from 'react-simple-star-rating';
---
+++ garage/src/components/Avis/Rating.js
@@ -0,0 +1,39 @@
+import "../../App.css";
+import { useState } from 'react'
+import { Rating } from 'react-simple-star-rating'
+
+const Stars = () => {
+    const [rating, setRating] = useState(100) // Va jusqu'à 100 donc la 5 étoile
+  
+   
+    const handleRating = (rate) => {
+      console.log(rate)
+      setRating(rate)
+      
+    }
+  
+    return (
+      <Rating
+        fillColor="#F0C300"
+        //allowHalfIcon //Pour les demi étoile, là, on est à true
+        // tooltipArray={['nul', 'bof', 'moyen', 'top', 'génial']}
+        transition
+        // showTooltip
+        onClick={handleRating}
+        ratingValue={rating} 
+      />
+    )
+  }
+  
+  const Star = () => {
+    return (
+      <div className="App">
+        <h1>Hello CodeSandbox</h1>
+        <h2>Start editing to see some magic happen!</h2>
+        <Stars />
+      </div>
+    )
+  }
+  
+  export default Stars;
+  
